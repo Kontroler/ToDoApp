4e202eb06dd664e22d65e21bf75003ae56d4fd80
Rafał Nowowieski <rafal.nowowieski@gmail.com>
2020-09-06 18:59:58 +0200

    Fixes #5

---
+++ ToDoApp.Domain/Models/ToDoItemStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Domain.Models
+{
+    public class ToDoItemStatus
+    {
+        public int Id { get; set; }
+        public StatusName Name { get; set; }
+
+        public enum StatusName
+        {
+            InProgress, Done
+        }
+    }
+}
