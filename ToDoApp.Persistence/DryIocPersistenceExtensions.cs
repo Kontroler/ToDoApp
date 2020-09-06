30647162df939631367ad9e1323a6fde28174c08
Rafał Nowowieski <rafal.nowowieski@gmail.com>
2020-09-06 07:38:51 +0200

    Fixes #3

---
+++ ToDoApp.Persistence/DryIocPersistenceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using DryIoc;
+using SQLite;
+using ToDoApp.Persistence.Managers;
+
+namespace ToDoApp.Persistence
+{
+    public static class DryIocPersistenceExtensions
+    {
+        public static void AddPersistence(this IContainer c)
+        {
+            c.Register<IToDoAppDatabase, ToDoAppDatabase>();
+            c.Register<IToDoPersistenceManager, ToDoPersistenceManager>();
+            c.Register<IToDoStatusPersistenceManager, ToDoStatusPerstistenceManager>();
+        }
+    }
+}
---
+++ ToDoApp.Persistence/IToDoAppDatabase.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Persistence
+{
+    public interface IToDoAppDatabase
+    {
+        SQLiteAsyncConnection Database { get; }
+    }
+}
